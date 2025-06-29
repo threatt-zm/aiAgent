af9488d81dca5eb73e6e0efcf9cdcb153fa0b48a
threatt-zm <10990825@live.mercer.edu>
2025-06-29 00:12:14 -0400

    update to allow --verbose optional flag

--- main.py
+++ main.py
@@ -2,18 +2,33 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-if len(sys.argv) < 2:
-    print("Please provide a prompt!")
-    sys.exit(1)
-else:
-    prompt = sys.argv[1]
+def main():
+    load_dotenv()
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+    verbose = "--verbose" in sys.argv
+    args = sys.argv[1:]
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if not args:
+        print("Please provide a prompt!")
+        sys.exit(1)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    prompt = args[0]
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)]),]
+    
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages,)
+
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
+    print(f"Response:\n{response.text}")
+    
+           
+
+if __name__ == "__main__":
+    main()
