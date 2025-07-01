f2ebc525f23e5471bad00c561cbe2df226dd5222
threatt-zm <10990825@live.mercer.edu>
2025-07-01 01:49:22 -0400

    program to list the contents of a file with test cases

---
+++ functions/get_file_content.py
@@ -0,0 +1,19 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    current_path = os.path.abspath(working_directory)
+    absolute_path = os.path.join(current_path, file_path)
+
+    if absolute_path != current_path + "/" + file_path:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(absolute_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(absolute_path, "r") as f:
+            file_content = f.read(10000)
+            if os.path.getsize(absolute_path) > 10000:
+                file_content += f'\n[...File "{file_path}" truncated at 10000 characters]'
+            return file_content
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def TestGetFilesInfo():
 
@@ -18,5 +19,23 @@ def TestGetFilesInfo():
     result = get_files_info("calculator", "../")
     print(result)
 
+def TestGetFileContent():
+
+    print("Contents of main.py file:")
+    result = get_file_content("calculator", "main.py")
+    print(result)
+    print("=========================================================")
+
+    print("Contents of pkg/calculator.py file:")
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
+    print("=========================================================")
+
+    print("Contents of /bin/cat file:")
+    result = get_file_content("calculator", "/bin/cat")
+    print(result)
+    print("=========================================================")
+
 if __name__ == "__main__":
-    TestGetFilesInfo()
+    #TestGetFilesInfo()
+    TestGetFileContent()
