c82f37832ebe089971de97cad48234f53fc202ff
threatt-zm <10990825@live.mercer.edu>
2025-07-01 03:57:07 -0400

    code update

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,9 +2,9 @@ import os
 
 def get_file_content(working_directory, file_path):
     current_path = os.path.abspath(working_directory)
-    absolute_path = os.path.join(current_path, file_path)
+    absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
 
-    if absolute_path != current_path + "/" + file_path:
+    if not absolute_path.startswith(current_path):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.isfile(absolute_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
